fb31531ed7ed3984df891c452158645bb0cd86fd
Miguel Seguame Reyes <seguame@gmail.com>
2013-03-18 17:55:55 -0600

    Mejora para la sugerencia de query

--- index.php
+++ index.php
@@ -1,30 +1,28 @@
 <?php
 
 	include ('Controller/controladorCliente.php');
-
-	switch ($_REQUEST['uso']) 
+	
+	if(isset($_REQUEST['uso']))
 	{
-		case 'cliente':
-			$controlador = new ControladorCliente();
-			break;
-		case 'Contrato':
-			//$controlador = asdasdas;
+		switch ($_REQUEST['uso']) 
+		{
+			case 'cliente':
+				$controlador = new ControladorCliente();
+				break;
+			case 'Contrato':
+				//$controlador = asdasdas;
+			
+			default:
+				die("'uso' invalido");
+		}
 		
-		default:
-			$invalido = TRUE;
-			break;
-	}
-
-	if(isset($invalido))
-	{
-		echo 'Ejemplos de uso: '.PHP_EOL;
-		
-		//Llamadas al controlador que se encarga de manejar al cliente
-		echo '?uso=cliente&accion=insertar&nombre=Miguel&apellidoPat=Seguame&apellidoMat=Reyes&RFC=1234567890123&telefonos=36436418&cuentasBancarias=012345678&emails=lol%40lol&domicilios=JesusUrueta&esPersonaFisica=TRUE'.PHP_EOL;
-	}
-	else 
-	{
 		$controlador->ejecutar();
 	}
+	
+	echo 'Ejemplos de uso: '.PHP_EOL;
+		
+	//Llamadas al controlador que se encarga de manejar al cliente
+	echo '?uso=cliente&accion=insertar&nombre=Miguel&apellidoPat=Seguame&apellidoMat=Reyes&RFC=1234567890123&telefonos=36436418&cuentasBancarias=012345678&emails=lol%40lol&domicilios=JesusUrueta&esPersonaFisica=TRUE'.PHP_EOL;
+	
 
 ?>
