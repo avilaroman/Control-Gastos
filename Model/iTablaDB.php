66e2f07485471bd2ce2e6dc2e769895f1b46677b
Miguel Seguame Reyes <seguame@gmail.com>
2013-03-18 17:43:24 -0600

    Añadidas mas funciones de Tabla
    Una tabla puede ser recuperada

--- Model/iTablaDB.php
+++ Model/iTablaDB.php
@@ -5,6 +5,7 @@ interface iTablaDB
     public function insertar($tabla);
     public function eliminar($tabla);
     public function modificar($tabla);
+	public function recuperar($tabla, $id);
 }
 
 
