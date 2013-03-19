7af8cef837db31e6559d945b91bd563ef31a9094
Miguel Seguame Reyes <seguame@gmail.com>
2013-03-18 20:29:54 -0600

    Index permite modulo de contrato

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 	include ('Controller/controladorCliente.php');
+	include ('Controller/ContratoCtl.php');
 	
 	if(isset($_REQUEST['uso']))
 	{
@@ -9,8 +10,9 @@
 			case 'cliente':
 				$controlador = new ControladorCliente();
 				break;
-			case 'Contrato':
-				//$controlador = asdasdas;
+			case 'contrato':
+				$controlador = new ControladorContrato();
+				break;
 			
 			default:
 				die("'uso' invalido");
@@ -18,11 +20,12 @@
 		
 		$controlador->ejecutar();
 	}
-	
-	echo 'Ejemplos de uso: '.PHP_EOL;
+	else
+	{
+		echo 'Ejemplos de uso: '.PHP_EOL;
 		
-	//Llamadas al controlador que se encarga de manejar al cliente
-	echo '?uso=cliente&accion=insertar&nombre=Miguel&apellidoPat=Seguame&apellidoMat=Reyes&RFC=1234567890123&telefonos=36436418&cuentasBancarias=012345678&emails=lol%40lol&domicilios=JesusUrueta&esPersonaFisica=TRUE'.PHP_EOL;
-	
+		//Llamadas al controlador que se encarga de manejar al cliente
+		echo '?uso=cliente&accion=insertar&nombre=Miguel&apellidoPat=Seguame&apellidoMat=Reyes&RFC=1234567890123&telefonos=36436418&cuentasBancarias=012345678&emails=lol%40lol&domicilios=JesusUrueta&esPersonaFisica=TRUE&username=lal&password=lal'.PHP_EOL;
+	}
 
 ?>
