59f0b712f801862055e42845ec5c9eafec0dde9b
Eduardo Esparza <neoraidenx@gmail.com>
2013-04-03 17:11:42 -0600

    Limpieza de variables de user y password

---
+++ Codigo/Model/Menu.php
@@ -0,0 +1,41 @@
+<?php
+
+include ('Controller/ClienteCtl.php');
+include ('Controller/ContratoCtl.php');
+include ('Controller/SessionCtl.php');
+    
+    if(isset($_REQUEST['uso']))
+    {
+        switch ($_REQUEST['uso']) 
+        {
+            case 'cliente':
+                $controlador = new ControladorCliente();
+                break;
+            case 'contrato':
+                $controlador = new ControladorContrato();
+                break;
+            case 'login':
+                $controlador = new SessionCtl();
+                break;
+            default:
+                die("'uso' invalido");
+        }
+        
+        $controlador->ejecutar();
+        if($controlador->valido==TRUE)
+            $_SESSION['usuario']=$controlador->model->nombre;
+    }
+    else
+    {
+        echo 'Ejemplos de uso: '.PHP_EOL;
+        
+        echo 'Para clientes'.PHP_EOL.PHP_EOL;
+        //Llamadas al controlador que se encarga de manejar al cliente
+        echo 'http://www.seguame.com/?uso=cliente&accion=insertar&nombre=Miguel&apellidoPat=Seguame&apellidoMat=Reyes&RFC=1234567890123&telefonos=36436418&nombreBanco=Santander&numeroCuenta=124a&emails=lol%40lol&calle=JesusUrueta&numInterior=1600&numExterior=1&colonia=Mirador&cp=4444&estado=Jalisco&municipio=gdl&esPersonaFisica=TRUE&username=lal&password=lal'.PHP_EOL;
+        
+        echo 'Para contrato'.PHP_EOL.PHP_EOL;
+        //Controlador de Contrato
+        echo '?uso=contrato&accion=crear&nombre=Miguel&apellidoPat=Seguame&apellidoMat=Reyes&RFC=1234567890123&telefonos=36436418&nombreBanco=Santander&numeroCuenta=124a&emails=lol%40lol&calle=JesusUrueta&numInterior=1600&numExterior=1&colonia=Mirador&cp=4444&estado=Jalisco&municipio=gdl&idCuenta=1&fecha=hoy&periodo=masd&presupuesto=1212&plazos=TRUE&renovacion=2432&saldado=FALSE';
+    }
+
+?>
--- Codigo/index.php
+++ Codigo/index.php
@@ -1,41 +1,55 @@
 <?php
-
-	include ('Controller/ClienteCtl.php');
-	include ('Controller/ContratoCtl.php');
-    include ('Controller/SessionCtl.php');
 	
-	if(isset($_REQUEST['uso']))
-	{
-		switch ($_REQUEST['uso']) 
-		{
-			case 'cliente':
-				$controlador = new ControladorCliente();
-				break;
-			case 'contrato':
-				$controlador = new ControladorContrato();
-				break;
-            case 'login':
-                $controlador = new SessionCtl();
-                break;
-			default:
-				die("'uso' invalido");
-		}
-		
-		$controlador->ejecutar();
-        if($controlador->valido==TRUE)
-            $_SESSION['usuario']=$controlador->model->nombre;
+    //#1
+    
+    /*
+     * Antes de cualquier cosa, incluso del html debemos de iniciar el objeto de sesion y este
+     * tiene que estar presente en todo el sitio
+     */
+    session_start();
+    
+    //verificamos que exista la sesion llama usuario y que no este vacia
+    if(isset($_SESSION['usuario']) && is_object($_SESSION['usuario'])){
+?>
+    <script language="javascript" type="text/javascript">
+                
+                /*php por si mismo no puede redireccionarse a otra pagina
+                *asi que esta sentencia en js nos hace ese trabajo
+                */
+                document.location.href="Menu.php";
+                //Este formulario estará disponible cuando se comience a trabajar con la BD + HTML
+     </script>
+        <?php
+    
+	}else{
+	    ?>
+	    <html>
+    <head>
+        <title>Login Control de Gastos</title>
+    </head>
+    
+    <body>
+        <form action="Menu.php" method="post" name="login">
+            <table width="300" align="center">
+                <tr>
+                    <input type="hidden" name="uso" value="login">
+                    <input type="hidden" name="accion" value="consultar">
+                    <td width="100"><label>Usuario: </label></td>
+                    <td width="200"><input type="text" width="200" name="username"/></td>
+                </tr>
+                <tr>
+                    <td width="100"><label>Contrase&ntilde;a</label></td>
+                    <td width="200"><input type="password" width="200" name="password" /></td>
+                </tr>
+                <tr>
+                    
+                    <td colspan="5" align="middle"><input type="submit" name="enviar" value="Enviar"/></td>
+                    <td colspan="5" align="middle"><input type="submit" name="Registrar" value="Registrar"/></td>
+                </tr>
+            </table>
+         </form>
+    </body>
+</html>
+        <?php
 	}
-	else
-	{
-		echo 'Ejemplos de uso: '.PHP_EOL;
-		
-		echo 'Para clientes'.PHP_EOL.PHP_EOL;
-		//Llamadas al controlador que se encarga de manejar al cliente
-		echo 'http://www.seguame.com/?uso=cliente&accion=insertar&nombre=Miguel&apellidoPat=Seguame&apellidoMat=Reyes&RFC=1234567890123&telefonos=36436418&nombreBanco=Santander&numeroCuenta=124a&emails=lol%40lol&calle=JesusUrueta&numInterior=1600&numExterior=1&colonia=Mirador&cp=4444&estado=Jalisco&municipio=gdl&esPersonaFisica=TRUE&username=lal&password=lal'.PHP_EOL;
-		
-		echo 'Para contrato'.PHP_EOL.PHP_EOL;
-		//Controlador de Contrato
-		echo '?uso=contrato&accion=crear&nombre=Miguel&apellidoPat=Seguame&apellidoMat=Reyes&RFC=1234567890123&telefonos=36436418&nombreBanco=Santander&numeroCuenta=124a&emails=lol%40lol&calle=JesusUrueta&numInterior=1600&numExterior=1&colonia=Mirador&cp=4444&estado=Jalisco&municipio=gdl&idCuenta=1&fecha=hoy&periodo=masd&presupuesto=1212&plazos=TRUE&renovacion=2432&saldado=FALSE';
-	}
-
 ?>
