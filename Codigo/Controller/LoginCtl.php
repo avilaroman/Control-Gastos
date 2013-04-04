f2078e1d2226b3d37a719a7d52fe0f62b95aabf2
Eduardo Esparza <neoraidenx@gmail.com>
2013-04-03 18:06:10 -0600

    Creacion de class Login close #18

--- Codigo/Controller/LoginCtl.php
+++ Codigo/Controller/LoginCtl.php
@@ -1,12 +1,19 @@
 <?php
 require('Utils/Cleaner.php');
 
-    session_start();
-            $modelo = new Cliente();
-            $_POST = Cleaner.LimpiarTodo($_POST);
-                $usuario = $modelo->recuperarCliente($_POST['username'], $_POST['password']);
-                if($usuario==FALSE)
-                    echo "Usuario y/o contraseña incorrectos ";
-                else
-                    $_SESSION['user']=$usuario;
+class LoginCtl{
+        
+    function ejecutar(){
+        session_start();
+        $modelo = new Cliente();
+        $_POST = Cleaner::LimpiarTodo($_POST);
+        $usuario = $modelo->recuperarCliente($_POST['username'], $_POST['password']);
+        if($usuario==FALSE)
+            echo "Usuario y/o contraseña incorrectos ";
+        else
+            $_SESSION['user']=$usuario;
+    }
+    
+           
+}
 ?>
