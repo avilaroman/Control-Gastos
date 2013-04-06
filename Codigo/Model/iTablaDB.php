afd45dfe1440ad0256d868df3c9065c3b06dbe21
Miguel Seguame Reyes <seguame@gmail.com>
2013-04-05 18:25:50 -0600

    Las tablas de BD tienen que saber us ID
    Se mantendrá el id de la tabla a la que se refiere el objeto actual, usable para modificaciones y eliminaciones

--- Codigo/Model/iTablaDB.php
+++ Codigo/Model/iTablaDB.php
@@ -4,6 +4,9 @@ require_once('baseDatos.php');
 
 abstract class iTablaDB extends BaseDatos
 {
+	protected $id;
+	
+	
     public abstract function insertar();
     public abstract function eliminar();
     public abstract function modificar();
