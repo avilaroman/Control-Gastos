78be01bbcce9c9132a7c6d6eadb9237ee76392c9
Miguel Seguame Reyes <seguame@gmail.com>
2013-04-05 19:34:05 -0600

    LimpiarTodo limpia recursivamente arreglos en arreglos

--- Codigo/Utils/Cleaner.php
+++ Codigo/Utils/Cleaner.php
@@ -3,15 +3,22 @@
 # res que pudieran comprometer
 # al servidor y/o al usuario
 class Cleaner{
-    public static function limpia($var){
-        $var = strip_tags($var);
-        $malo = array("\\",";","\'","'"); // Aqui poner caracteres no permitidos
-    
-        foreach($malo as $quitable){
-            $var = str_replace($quitable,"",$var);
-        }
-
-        return $var;
+    public static function limpia($var)
+    {
+    	if(is_array($var))
+		{
+			Cleaner::LimpiarTodo($var);
+		}
+		else
+		{
+			$var = strip_tags($var);
+	        $malo = array("\\",";","\'","'"); // Aqui poner caracteres no permitidos
+	    
+	        foreach($malo as $quitable){
+	            $var = str_replace($quitable,"",$var);
+	        }
+	        return $var;
+		}
     }
  
 # Funcion que aplica la funcion anterior
