5f7d51dcaf2d7a57023a4506c9e8f8c14b1cfb3d
Miguel Seguame Reyes <seguame@gmail.com>
2013-04-05 19:36:38 -0600

    Telefonos, direcciones y correos por separado cierra #14
    Las insercciones de estos elementos se realizan de modo independiente

--- Codigo/Controller/ClienteCtl.php
+++ Codigo/Controller/ClienteCtl.php
@@ -5,44 +5,90 @@ require('Model/cliente.php');
 class ControladorCliente
 {
 	public $modelo;
-	private $accionador;
 	
-	function ejecutar()
+	public function ejecutar()
 	{
 		//si no hya parametros, nomas listar los usuarios
 		if(!isset($_REQUEST['accion']) )
 		{
 			die('No se definio que accion tomar');	
 		}
-		else{
-		    $_REQUEST = Cleaner::LimpiarTodo($_REQUEST);
-             switch ($_REQUEST['accion']) 
+		else
 		{
-			case 'insertar':
-				$nombre = $_REQUEST['nombre'];
-				$apellidoPat = $_REQUEST['apellidoPat'];
-				$apellidoMat = $_REQUEST['apellidoMat'];
-				$RFC = $_REQUEST['RFC'];
-				$esPersonaFisica = $_REQUEST['esPersonaFisica'];
-				
-				$this->modelo = new Cliente($nombre, $apellidoPat, $apellidoMat, $RFC, $esPersonaFisica);
-				
-				$usuario = $this->modelo;
-				$idBase = $this->modelo->insertar();
-				$this->modelo->crearCliente($idBase);
-				
-				break;
-
-			default:
-				echo 'DAFUQ AR U DOIN =_=';
-				$usuario = 'groar*';
-				break;
-		}
+		    $_REQUEST = Cleaner::LimpiarTodo($_REQUEST);
+			
+            switch ($_REQUEST['accion']) 
+			{
+				case 'insertar':
+					$this->InsertarCliente();
+					break;
+	
+				default:
+					echo 'DAFUQ AR U DOIN =_=';
+					$usuario = 'groar*';
+					break;
+			}
         }
 
 		include ('View/vista.php');
 
 	}
+	
+	private function InsertarCliente()
+	{
+		$nombre = $_REQUEST['nombre'];
+		$apellidoPat = $_REQUEST['apellidoPat'];
+		$apellidoMat = $_REQUEST['apellidoMat'];
+		$RFC = $_REQUEST['RFC'];
+		$esPersonaFisica = $_REQUEST['esPersonaFisica'];
+			
+		$this->modelo = new Cliente($nombre, $apellidoPat, $apellidoMat, $RFC, $esPersonaFisica);
+					
+		$usuario = $this->modelo;
+		if(!$this->modelo->insertar())
+		{
+			die('ERRORES CREANDO AL CLIENTE');
+		}
+		
+		$this->modelo->crearCliente($idCliente);
+		
+		if(isset($_REQUEST['telefono']))
+		{
+			$telefono = new Telefono($this->modelo->getIdEntidad(), $_REQUEST['telefono']);
+			$telefono->insertar();
+		}
+
+		if(isset($_REQUEST['email']))
+		{
+			$email = new Telefono($this->modelo->getIdEntidad(), $_REQUEST['email']);
+			$email->insertar();
+		}
+		
+		if(isset($_REQUEST['direccion']))
+		{
+			$this->InsertarDireccion();
+		}
+		
+	}
+
+	private function InsertarDireccion()
+	{
+		//direccion es un arreglo
+		$tmp = $_REQUEST['direccion'];
+		Cleaner::LimpiarTodo($tmp);
+			
+			
+		$calle 			= $tmp['calle'];
+		$numInterior 	= $tmp['numInterior'];
+		$numExterior 	= $tmp['numExterior'];
+		$colonia 		= $tmp['colonia'];
+		$codigoPostal 	= $tmp['cp'];
+		$estado 		= $tmp['estado'];
+		$municipio 		= $tmp['municipio'];
+		
+		$direccion = new Direccion($this->modelo->getIdEntidad(), $calle, $numInterior, $numExterior, $colonia, $codigoPostal, $estado, $municipio);
+		$direccion->insertar();
+	} 
 }
 
 ?>
