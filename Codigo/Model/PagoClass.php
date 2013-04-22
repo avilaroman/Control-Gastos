48fed042b6bf1d10c54371fed3c59c4aadf7c61f
Eduardo Esparza <neoraidenx@gmail.com>
2013-04-21 19:17:05 -0500

    Cambio de funciones a su respectiva clase para cumplir con iTablaBD

--- Codigo/Model/PagoClass.php
+++ Codigo/Model/PagoClass.php
@@ -36,5 +36,101 @@ class PagoClass{
 
 		$BD->cerrar_conexion();
 	}
+    
+    function modificar($campo, $valor, $id){
+        $this->conecta();
+        if(!$this->conecta())
+        {
+            die('SHIT HAPPENS: '.$this->conexion->errno.':'.$this->conexion->error);
+        }
+        $query = "UPDATE
+                    Pago
+                    SET
+                    $campo = '$valor'
+                    WHERE
+                    id_pago = '$id'";
+        $resultado = $this->conexion->query($query);
+        
+        if($this->conexion->errno)
+        {
+            echo 'Error modificando Pago '.$this->conexion->errno.' : '.$this->conexion->error;
+            //Cerrar la conexion
+            $this->cerrar_conexion();
+            return FALSE;
+        }
+        else
+        {
+            //Cerrar la conexion
+            $this->cerrar_conexion();
+            printf ("Campo Modificado \n");    
+        }
+        
+    }
+    
+    function eliminar($id){
+        $this->conecta();
+        if(!$this->conecta())
+        {
+            die('SHIT HAPPENS: '.$this->conexion->errno.':'.$this->conexion->error);
+        }
+        $query = "DELETE FROM
+                    Pago
+                    WHERE
+                    id_pago LIKE '$id'";
+        $resultado = $this->conexion->query($query);
+        
+        if($this->conexion->errno)
+        {
+            echo 'Error eliminando Pago '.$this->conexion->errno.' : '.$this->conexion->error;
+            //Cerrar la conexion
+            $this->cerrar_conexion();
+            return FALSE;
+        }
+        else
+        {
+            //Cerrar la conexion
+            $this->cerrar_conexion();
+            printf ("Pago Eliminado \n");    
+        }
+        
+        
+    }
+    
+    function ConsultarPago(){
+        
+    $this->conecta();
+        if(!$this->conecta())
+        {
+            die('SHIT HAPPENS: '.$this->conexion->errno.':'.$this->conexion->error);
+        }
+        $query = "SELECT 
+                    *
+                 FROM
+                    Pago";
+        $resultado = $this->conexion->query($query);
+        
+        if($this->conexion->errno)
+        {
+            echo 'Error consultando Pago '.$this->conexion->errno.' : '.$this->conexion->error;
+            //Cerrar la conexion
+            $this->cerrar_conexion();
+            return FALSE;
+        }
+        else
+        {
+            //Cerrar la conexion
+            $this->cerrar_conexion();
+
+            while ($fila = $resultado -> fetch_assoc())
+            printf ("[%s|%s|%s]\n", $fila["id_pago"], $fila["monto"], $fila["fecha_pago"]);    
+        }
+        
+        
+    }
+    
+    function insertar($montoP,$fechaP){
+        $model = new PagoClass($montoP,$fechaP);
+    }
+    
 }
 ?>
