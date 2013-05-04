d7a2feb1b765eaf81062634e714ba2fa58fb6e72
Miguel Seguame Reyes <seguame@gmail.com>
2013-05-03 19:42:01 -0500

    Script SQL actualizado pra funcionar en alanturing

--- Documentacion/Diagrama BD/Control_Gastos.sql
+++ Documentacion/Diagrama BD/Control_Gastos.sql
@@ -2,13 +2,12 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `Control_Gastos` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci ;
-USE `Control_Gastos` ;
+USE `cc409_gastosse` ;
 
 -- -----------------------------------------------------
--- Table `Control_Gastos`.`Entidad`
+-- Table `cc409_gastosse`.`Entidad`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Entidad` (
+CREATE  TABLE IF NOT EXISTS `cc409_gastosse`.`Entidad` (
   `id_entidad` INT NOT NULL AUTO_INCREMENT ,
   `nombre` VARCHAR(45) NOT NULL ,
   `apellido_paterno` VARCHAR(45) NULL ,
@@ -19,9 +18,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Control_Gastos`.`Cliente`
+-- Table `cc409_gastosse`.`Cliente`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Cliente` (
+CREATE  TABLE IF NOT EXISTS `cc409_gastosse`.`Cliente` (
   `id_cliente` INT NOT NULL AUTO_INCREMENT ,
   `Entidad_id_entidad` INT NOT NULL ,
   `persona_fisica` TINYINT(1) NOT NULL ,
@@ -29,16 +28,16 @@ CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Cliente` (
   INDEX `fk_Cliente_Entidad1_idx` (`Entidad_id_entidad` ASC) ,
   CONSTRAINT `fk_Cliente_Entidad1`
     FOREIGN KEY (`Entidad_id_entidad` )
-    REFERENCES `Control_Gastos`.`Entidad` (`id_entidad` )
+    REFERENCES `cc409_gastosse`.`Entidad` (`id_entidad` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Control_Gastos`.`Cuenta`
+-- Table `cc409_gastosse`.`Cuenta`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Cuenta` (
+CREATE  TABLE IF NOT EXISTS `cc409_gastosse`.`Cuenta` (
   `id_cuenta` INT NOT NULL AUTO_INCREMENT ,
   `Cliente_id_cliente` INT NOT NULL ,
   `nombre_usuario` VARCHAR(30) NOT NULL ,
@@ -48,16 +47,16 @@ CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Cuenta` (
   INDEX `fk_Cuenta_Cliente1_idx` (`Cliente_id_cliente` ASC) ,
   CONSTRAINT `fk_Cuenta_Cliente1`
     FOREIGN KEY (`Cliente_id_cliente` )
-    REFERENCES `Control_Gastos`.`Cliente` (`id_cliente` )
+    REFERENCES `cc409_gastosse`.`Cliente` (`id_cliente` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Control_Gastos`.`Contrato`
+-- Table `cc409_gastosse`.`Contrato`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Contrato` (
+CREATE  TABLE IF NOT EXISTS `cc409_gastosse`.`Contrato` (
   `id_contrato` INT NOT NULL AUTO_INCREMENT ,
   `Cuenta_id_cuenta` INT NOT NULL ,
   `Entidad_id_contacto` INT NOT NULL ,
@@ -72,21 +71,21 @@ CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Contrato` (
   INDEX `fk_Contrato_Entidad1_idx` (`Entidad_id_contacto` ASC) ,
   CONSTRAINT `fk_Contrato_Cuenta1`
     FOREIGN KEY (`Cuenta_id_cuenta` )
-    REFERENCES `Control_Gastos`.`Cuenta` (`id_cuenta` )
+    REFERENCES `cc409_gastosse`.`Cuenta` (`id_cuenta` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Contrato_Entidad1`
     FOREIGN KEY (`Entidad_id_contacto` )
-    REFERENCES `Control_Gastos`.`Entidad` (`id_entidad` )
+    REFERENCES `cc409_gastosse`.`Entidad` (`id_entidad` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Control_Gastos`.`Domicilio`
+-- Table `cc409_gastosse`.`Domicilio`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Domicilio` (
+CREATE  TABLE IF NOT EXISTS `cc409_gastosse`.`Domicilio` (
   `id_domicilio` INT NOT NULL AUTO_INCREMENT ,
   `calle` VARCHAR(45) NOT NULL ,
   `num_interior` INT NULL DEFAULT NULL ,
@@ -100,9 +99,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Control_Gastos`.`Cuenta_Bancaria`
+-- Table `cc409_gastosse`.`Cuenta_Bancaria`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Cuenta_Bancaria` (
+CREATE  TABLE IF NOT EXISTS `cc409_gastosse`.`Cuenta_Bancaria` (
   `id_cuenta_Bancaria` INT NOT NULL AUTO_INCREMENT ,
   `Entidad_id_entidad` INT NOT NULL ,
   `num_cuenta` VARCHAR(45) NOT NULL ,
@@ -110,16 +109,16 @@ CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Cuenta_Bancaria` (
   INDEX `fk_Cuenta_Bancaria_Entidad1_idx` (`Entidad_id_entidad` ASC) ,
   CONSTRAINT `fk_Cuenta_Bancaria_Entidad1`
     FOREIGN KEY (`Entidad_id_entidad` )
-    REFERENCES `Control_Gastos`.`Entidad` (`id_entidad` )
+    REFERENCES `cc409_gastosse`.`Entidad` (`id_entidad` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Control_Gastos`.`Telefono`
+-- Table `cc409_gastosse`.`Telefono`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Telefono` (
+CREATE  TABLE IF NOT EXISTS `cc409_gastosse`.`Telefono` (
   `id_telefono` INT NOT NULL AUTO_INCREMENT ,
   `Entidad_id_entidad` INT NOT NULL ,
   `telefono` VARCHAR(45) NOT NULL ,
@@ -127,16 +126,16 @@ CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Telefono` (
   INDEX `fk_Telefono_Entidad1_idx` (`Entidad_id_entidad` ASC) ,
   CONSTRAINT `fk_Telefono_Entidad1`
     FOREIGN KEY (`Entidad_id_entidad` )
-    REFERENCES `Control_Gastos`.`Entidad` (`id_entidad` )
+    REFERENCES `cc409_gastosse`.`Entidad` (`id_entidad` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Control_Gastos`.`Email`
+-- Table `cc409_gastosse`.`Email`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Email` (
+CREATE  TABLE IF NOT EXISTS `cc409_gastosse`.`Email` (
   `id_email` INT NOT NULL AUTO_INCREMENT ,
   `Entidad_id_entidad` INT NOT NULL ,
   `email` VARCHAR(45) NOT NULL ,
@@ -144,16 +143,16 @@ CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Email` (
   INDEX `fk_Email_Entidad1_idx` (`Entidad_id_entidad` ASC) ,
   CONSTRAINT `fk_Email_Entidad1`
     FOREIGN KEY (`Entidad_id_entidad` )
-    REFERENCES `Control_Gastos`.`Entidad` (`id_entidad` )
+    REFERENCES `cc409_gastosse`.`Entidad` (`id_entidad` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Control_Gastos`.`Gasto`
+-- Table `cc409_gastosse`.`Gasto`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Gasto` (
+CREATE  TABLE IF NOT EXISTS `cc409_gastosse`.`Gasto` (
   `id_gasto` INT NOT NULL AUTO_INCREMENT ,
   `Contrato_id_contrato` INT NOT NULL ,
   `costo` DECIMAL NOT NULL ,
@@ -167,16 +166,16 @@ CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Gasto` (
   INDEX `fk_Gasto_Contrato1_idx` (`Contrato_id_contrato` ASC) ,
   CONSTRAINT `fk_Gasto_Contrato1`
     FOREIGN KEY (`Contrato_id_contrato` )
-    REFERENCES `Control_Gastos`.`Contrato` (`id_contrato` )
+    REFERENCES `cc409_gastosse`.`Contrato` (`id_contrato` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Control_Gastos`.`Modo`
+-- Table `cc409_gastosse`.`Modo`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Modo` (
+CREATE  TABLE IF NOT EXISTS `cc409_gastosse`.`Modo` (
   `id_modo_pago` INT NOT NULL AUTO_INCREMENT ,
   `nombre` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`id_modo_pago`) )
@@ -184,9 +183,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Control_Gastos`.`Pago`
+-- Table `cc409_gastosse`.`Pago`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Pago` (
+CREATE  TABLE IF NOT EXISTS `cc409_gastosse`.`Pago` (
   `id_pago` INT NOT NULL AUTO_INCREMENT ,
   `Contrato_id_contrato` INT NOT NULL ,
   `monto` DECIMAL NOT NULL ,
@@ -195,16 +194,16 @@ CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Pago` (
   INDEX `fk_Pago_Contrato1_idx` (`Contrato_id_contrato` ASC) ,
   CONSTRAINT `fk_Pago_Contrato1`
     FOREIGN KEY (`Contrato_id_contrato` )
-    REFERENCES `Control_Gastos`.`Contrato` (`id_contrato` )
+    REFERENCES `cc409_gastosse`.`Contrato` (`id_contrato` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Control_Gastos`.`Asunto`
+-- Table `cc409_gastosse`.`Asunto`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Asunto` (
+CREATE  TABLE IF NOT EXISTS `cc409_gastosse`.`Asunto` (
   `id_asunto` INT NOT NULL AUTO_INCREMENT ,
   `asunto` VARCHAR(255) NOT NULL ,
   PRIMARY KEY (`id_asunto`) )
@@ -212,9 +211,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Control_Gastos`.`Gasto_has_Modo_Pago`
+-- Table `cc409_gastosse`.`Gasto_has_Modo_Pago`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Gasto_has_Modo_Pago` (
+CREATE  TABLE IF NOT EXISTS `cc409_gastosse`.`Gasto_has_Modo_Pago` (
   `Gasto_id_gasto` INT NOT NULL ,
   `Modo_Pago_id_modo_pago` INT NOT NULL ,
   PRIMARY KEY (`Gasto_id_gasto`, `Modo_Pago_id_modo_pago`) ,
@@ -222,21 +221,21 @@ CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Gasto_has_Modo_Pago` (
   INDEX `fk_Gasto_has_Modo_Pago_Gasto1_idx` (`Gasto_id_gasto` ASC) ,
   CONSTRAINT `fk_Gasto_has_Modo_Pago_Gasto1`
     FOREIGN KEY (`Gasto_id_gasto` )
-    REFERENCES `Control_Gastos`.`Gasto` (`id_gasto` )
+    REFERENCES `cc409_gastosse`.`Gasto` (`id_gasto` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Gasto_has_Modo_Pago_Modo_Pago1`
     FOREIGN KEY (`Modo_Pago_id_modo_pago` )
-    REFERENCES `Control_Gastos`.`Modo` (`id_modo_pago` )
+    REFERENCES `cc409_gastosse`.`Modo` (`id_modo_pago` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Control_Gastos`.`Pago_has_Modo`
+-- Table `cc409_gastosse`.`Pago_has_Modo`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Pago_has_Modo` (
+CREATE  TABLE IF NOT EXISTS `cc409_gastosse`.`Pago_has_Modo` (
   `Pago_id_pago` INT NOT NULL ,
   `Modo_id_modo_pago` INT NOT NULL ,
   PRIMARY KEY (`Pago_id_pago`, `Modo_id_modo_pago`) ,
@@ -244,21 +243,21 @@ CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Pago_has_Modo` (
   INDEX `fk_Pago_has_Modo_Pago1_idx` (`Pago_id_pago` ASC) ,
   CONSTRAINT `fk_Pago_has_Modo_Pago1`
     FOREIGN KEY (`Pago_id_pago` )
-    REFERENCES `Control_Gastos`.`Pago` (`id_pago` )
+    REFERENCES `cc409_gastosse`.`Pago` (`id_pago` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Pago_has_Modo_Modo1`
     FOREIGN KEY (`Modo_id_modo_pago` )
-    REFERENCES `Control_Gastos`.`Modo` (`id_modo_pago` )
+    REFERENCES `cc409_gastosse`.`Modo` (`id_modo_pago` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Control_Gastos`.`Contrato_has_Asunto`
+-- Table `cc409_gastosse`.`Contrato_has_Asunto`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Contrato_has_Asunto` (
+CREATE  TABLE IF NOT EXISTS `cc409_gastosse`.`Contrato_has_Asunto` (
   `Contrato_id_contrato` INT NOT NULL ,
   `Asunto_id_asunto` INT NOT NULL ,
   `presupuestado` TINYINT(1) NOT NULL ,
@@ -267,21 +266,21 @@ CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Contrato_has_Asunto` (
   INDEX `fk_Contrato_has_Asunto_Contrato1_idx` (`Contrato_id_contrato` ASC) ,
   CONSTRAINT `fk_Contrato_has_Asunto_Contrato1`
     FOREIGN KEY (`Contrato_id_contrato` )
-    REFERENCES `Control_Gastos`.`Contrato` (`id_contrato` )
+    REFERENCES `cc409_gastosse`.`Contrato` (`id_contrato` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Contrato_has_Asunto_Asunto1`
     FOREIGN KEY (`Asunto_id_asunto` )
-    REFERENCES `Control_Gastos`.`Asunto` (`id_asunto` )
+    REFERENCES `cc409_gastosse`.`Asunto` (`id_asunto` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Control_Gastos`.`Entidad_has_Domicilio`
+-- Table `cc409_gastosse`.`Entidad_has_Domicilio`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Entidad_has_Domicilio` (
+CREATE  TABLE IF NOT EXISTS `cc409_gastosse`.`Entidad_has_Domicilio` (
   `Entidad_id_entidad` INT NOT NULL ,
   `Domicilio_id_domicilio` INT NOT NULL ,
   PRIMARY KEY (`Entidad_id_entidad`, `Domicilio_id_domicilio`) ,
@@ -289,21 +288,21 @@ CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Entidad_has_Domicilio` (
   INDEX `fk_Entidad_has_Domicilio_Entidad1_idx` (`Entidad_id_entidad` ASC) ,
   CONSTRAINT `fk_Entidad_has_Domicilio_Entidad1`
     FOREIGN KEY (`Entidad_id_entidad` )
-    REFERENCES `Control_Gastos`.`Entidad` (`id_entidad` )
+    REFERENCES `cc409_gastosse`.`Entidad` (`id_entidad` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Entidad_has_Domicilio_Domicilio1`
     FOREIGN KEY (`Domicilio_id_domicilio` )
-    REFERENCES `Control_Gastos`.`Domicilio` (`id_domicilio` )
+    REFERENCES `cc409_gastosse`.`Domicilio` (`id_domicilio` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Control_Gastos`.`Banco`
+-- Table `cc409_gastosse`.`Banco`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Banco` (
+CREATE  TABLE IF NOT EXISTS `cc409_gastosse`.`Banco` (
   `id_banco` INT NOT NULL AUTO_INCREMENT ,
   `nombre` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`id_banco`) )
@@ -311,9 +310,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Control_Gastos`.`Cuenta_Bancaria_has_Banco`
+-- Table `cc409_gastosse`.`Cuenta_Bancaria_has_Banco`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Cuenta_Bancaria_has_Banco` (
+CREATE  TABLE IF NOT EXISTS `cc409_gastosse`.`Cuenta_Bancaria_has_Banco` (
   `Cuenta_Bancaria_id_cuenta_Bancaria` INT NOT NULL ,
   `Banco_id_banco` INT NOT NULL ,
   PRIMARY KEY (`Cuenta_Bancaria_id_cuenta_Bancaria`, `Banco_id_banco`) ,
@@ -321,17 +320,17 @@ CREATE  TABLE IF NOT EXISTS `Control_Gastos`.`Cuenta_Bancaria_has_Banco` (
   INDEX `fk_Cuenta_Bancaria_has_Banco_Cuenta_Bancaria1_idx` (`Cuenta_Bancaria_id_cuenta_Bancaria` ASC) ,
   CONSTRAINT `fk_Cuenta_Bancaria_has_Banco_Cuenta_Bancaria1`
     FOREIGN KEY (`Cuenta_Bancaria_id_cuenta_Bancaria` )
-    REFERENCES `Control_Gastos`.`Cuenta_Bancaria` (`id_cuenta_Bancaria` )
+    REFERENCES `cc409_gastosse`.`Cuenta_Bancaria` (`id_cuenta_Bancaria` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Cuenta_Bancaria_has_Banco_Banco1`
     FOREIGN KEY (`Banco_id_banco` )
-    REFERENCES `Control_Gastos`.`Banco` (`id_banco` )
+    REFERENCES `cc409_gastosse`.`Banco` (`id_banco` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-USE `Control_Gastos` ;
+USE `cc409_gastosse` ;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
