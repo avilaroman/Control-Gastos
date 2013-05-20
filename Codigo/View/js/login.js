c59a320a7066a44725ca4a2b7e936a4ff0616732
Miguel Seguame Reyes <seguame@gmail.com>
2013-05-19 22:47:45 -0500

    Añadido la funcion que se activa cuando tenemos lista la respuesta del servidor

--- Codigo/View/js/login.js
+++ Codigo/View/js/login.js
@@ -1,15 +1,22 @@
 function llenarEstados(){
 	var ajax = nuevoAjax();
-	ajax.open('post','Utils/GetEstados.php',true);
-	var response = eval(ajax.responseText);
-	console.log(response);
-	var estado = document.getElementById('selectEstados');
-	for (estado in response){
-		var option = document.createElement('option');
-		var text = document.createTextNode(response[estado].estado);
-		option.appenChild(text);
-		option.setAttribute('value',response[estado].id_estado);
-		estado.appendChild(option);
+	ajax.open('POST','Utils/GetEstados.php',true);
+	
+	ajax.onreadystatechange = function()
+	{
+		if(ajax.readyState == 4)
+		{
+			var response = eval(ajax.responseText);
+			for (estado in response){
+				var option = document.createElement('option');
+				var text = document.createTextNode(response[estado].estado);
+				option.appendChild(text);
+				option.setAttribute('value',response[estado].id_estado);
+				var estados = document.getElementById('selectEstados');
+				estados.appendChild(option);
+			}
+		}
 	}
+	
 	ajax.send(null);
 }
