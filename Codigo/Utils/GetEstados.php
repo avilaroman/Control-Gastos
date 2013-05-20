1d1432ee9665c113eee825d96d3bfe1dc1c40fdd
Miguel Seguame Reyes <seguame@gmail.com>
2013-05-19 22:20:28 -0500

    Corregida ruta de GetEstado

--- Codigo/Utils/GetEstados.php
+++ Codigo/Utils/GetEstados.php
@@ -1,9 +1,11 @@
 <?php
-require_once('../Model/EstadosMunicipio.php');
-
+require_once('../Model/EstadoMunicipio.php');
+//require_once('DebugerPHP.php');
 $Getter = new Localidades();
 
 $estados = $Getter->obtenEstados();
+
+//logConsole('$estados var', $estados, true);
 
 echo json_encode($estados);
 
