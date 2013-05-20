7efde87cd1a0e6b84ec246ac1d287459b3264241
Miguel Seguame Reyes <seguame@gmail.com>
2013-05-19 22:21:55 -0500

    Corregido error de ruta

--- Codigo/View/js/login.js
+++ Codigo/View/js/login.js
@@ -1,9 +1,8 @@
 function llenarEstados(){
 	var ajax = nuevoAjax();
-	ajax.open('post','GetEstados.php',true);
+	ajax.open('post','Utils/GetEstados.php',true);
 	var response = eval(ajax.responseText);
 	console.log(response);
-	console.log(response[0].id);
 	var estado = document.getElementById('selectEstados');
 	for (estado in response){
 		var option = document.createElement('option');
