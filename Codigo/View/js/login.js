30ac55509cfa77dd147c2b846245d0356f9d15f0
Eduardo Esparza <neoraidenx@gmail.com>
2013-05-19 20:39:18 -0500

    cambios random

--- Codigo/View/js/login.js
+++ Codigo/View/js/login.js
@@ -4,12 +4,12 @@ function llenarEstados(){
 	var response = eval(ajax.responseText);
 	console.log(response);
 	console.log(response[0].id);
+	var estado = document.getElementById('selectEstados');
 	for (estado in response){
 		var option = document.createElement('option');
 		var text = document.createTextNode(response[estado].estado);
 		option.appenChild(text);
 		option.setAttribute('value',response[estado].id_estado);
-		var estado = document.getElementById('estado');
 		estado.appendChild(option);
 	}
 	ajax.send(null);
