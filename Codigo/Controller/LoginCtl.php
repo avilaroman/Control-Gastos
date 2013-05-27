d03e0a825474d4d3b702432b4e383981f4112192
Miguel Seguame Reyes <seguame@gmail.com>
2013-05-27 01:21:09 -0500

    Login se cambia a $_REQUEST en lugar de $_POST

--- Codigo/Controller/LoginCtl.php
+++ Codigo/Controller/LoginCtl.php
@@ -1,26 +1,33 @@
 <?php
 require_once('Utils/Cleaner.php');
+//require_once('Utils/DebugerPHP.php');
 
 class LoginCtl{
         
     function ejecutar()
     {
-        if((isset($_SESSION['username'])))
+        if((isset($_SESSION)))
 		{
 			session_start();
 		}
         $modelo = new Cliente();
-        $_POST = Cleaner::LimpiarTodo($_POST);
-        $usuario = $modelo->recuperarCliente($_POST['username'], $_POST['password']);
+        $_REQUEST = Cleaner::LimpiarTodo($_REQUEST);
+        $usuario = $modelo->recuperarCliente($_REQUEST['username'], $_REQUEST['password']);
         
         if($usuario==FALSE)
 		{
             echo "Usuario y/o contraseña incorrectos ";
+			//logConsole("ERROR inicio sesion", $usuario);
 		}
         else
 		{
-			$_SESSION['username']=$usuario->getName();
-			$_SESSION['id'] = $usuario->getIdCliente();
+			$_SESSION['USERNAME'] = $_REQUEST['username'];
+			$_SESSION['PASSWORD'] = $_REQUEST['password'];
+			$_SESSION['username']=$modelo->getName();
+			$_SESSION['id'] = $modelo->getIdCliente();
+			
+			//var_dump($modelo);
+			//logConsole("SESSION", $_SESSION, true);
 		}
             
     }
